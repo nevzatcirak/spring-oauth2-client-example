2b5fdfbdfdf3cf46305648f56b394868c10d9aad
NvztCRK <nevzatcirak17@hotmail.com>
2019-12-27 15:30:36 +0300

    project structure is fixed.

--- spring-boot-react-oauth2-login-demo/react-app/src/util/APIUtils.js
+++ spring-boot-react-oauth2-login-demo/react-app/src/util/APIUtils.js
@@ -11,7 +11,7 @@ const request = (options) => {
 
     const defaults = {headers: headers};
     options = Object.assign({}, defaults, options);
-
+    
     return fetch(options.url, options)
     .then(response => 
         response.json().then(json => {
