a1b9fbc0eb466908492bc4efd0b6015eecf8bbe4
NvztCRK <nevzatcirak17@hotmail.com>
2019-12-27 11:37:42 +0300

    project structure is fixed.

--- spring-boot-react-oauth2-login-demo/src/main/java/com/nevzatcirak/example/SpringDemoApplication.java
+++ spring-boot-react-oauth2-login-demo/src/main/java/com/nevzatcirak/example/SpringDemoApplication.java
@@ -1,14 +1,15 @@
 package com.nevzatcirak.example;
 
 import com.nevzatcirak.example.config.AppProperties;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
-public class SpringApplication {
+public class SpringDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringApplication.class, args);
+		SpringApplication.run(SpringDemoApplication.class, args);
 	}
 }
--- spring-boot-react-oauth2-login-demo/src/test/java/com/nevzatcirak/example/SpringDemoApplicationTests.java
+++ spring-boot-react-oauth2-login-demo/src/test/java/com/nevzatcirak/example/SpringDemoApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-public class SpringApplicationTests {
+public class SpringDemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
